58d00c9bd0414c24e1323943bb5359fbed814728
Tristan van Wyk <tristanvwyk@gmail.com>
2020-03-07 17:12:35 +0200

    Terain and Object spawning

--- Task 1/New Unity Project/Assets/Scripts/SpawnObsacle.cs
+++ Task 1/New Unity Project/Assets/Scripts/SpawnObsacle.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class SpawnObsacle : MonoBehaviour
 {
+    public GameObject barrSmall;
+    public GameObject barrLarge;
+    public GameObject spike;
+    int[,] obstacles;
+    
+   
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        obstacles = new int[5, 3];
+
+
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                GameObject spawned;
+                obstacles[x, y] = Random.Range(0, 30);
+
+                Vector3 pos = new Vector3(this.transform.position.x - 4 +(x*2), 0, this.transform.position.z + 2 - (y*2));
+
+                switch (obstacles[x,y])
+                {
+                    case 10:
+                        spawned = Object.Instantiate(spike, pos, Quaternion.identity);
+                        spawned.transform.SetParent(this.transform);
+                        break;
+                    case 20:
+                        spawned = Object.Instantiate(barrLarge, pos, Quaternion.identity);
+                        spawned.transform.SetParent(this.transform);
+                        break;
+                    case 15:
+                        spawned = Object.Instantiate(barrSmall, pos, Quaternion.identity);
+                        spawned.transform.SetParent(this.transform);
+                        break;
+
+                    default:
+                        break;
+                        
+                }
+            }
+        }
+
+        for (int j = 0; j < 3; j++)
+        {
+            string adx = "";
+            for (int i = 0; i < 5; i++)
+            {
+                adx += obstacles[i, j] + "-";
+
+
+            }
+            Debug.Log(j + ":" + adx);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
         
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawCube(transform.position, Vector3.one);
+
     }
 }
--- Task 1/New Unity Project/Assets/Scripts/SpawnWalls.cs
+++ Task 1/New Unity Project/Assets/Scripts/SpawnWalls.cs
@@ -7,49 +7,60 @@ public class SpawnWalls : MonoBehaviour
     float Xoff;
     float Zoff;
 
+    public int[,] heights = new int[12, 6];
+    int lastHeight = -1;
+
     Vector2 dimentions = new Vector2(12, 6);
     public GameObject varblock;
-    
-    public float scale;
-    public int amplitude;
+    public int maxHeight;
     Transform[,] blocks;
     // Start is called before the first frame update
     void Start()
     {
 
-        float Xoff = transform.localPosition.x;
-        float Zoff = transform.localPosition.z;
+        float Xoff = transform.position.x;
+        float Zoff = transform.position.z;
+        
         // 12x6
         blocks = new Transform[12, 6];
+        
 
-        for (int x = 0; x < dimentions.x; x++)
+        for (int y = 0; y < dimentions.y; y++)
         {
-            for (int y = 0; y < dimentions.y; y++)
+            for (int x = 0; x < dimentions.x; x++)
             {
-                
-                int perlin = Mathf.Clamp(((int)((Mathf.PerlinNoise(x * scale +Time.fixedTime, y * scale+ Time.fixedTime)) *10)),0,amplitude);
-                perlin = (perlin * -1) + amplitude;
-                Debug.Log(""+perlin);
+                //heights[x, y] = Random.Range(0, maxHeight);
 
-                blocks[x, y] = Object.Instantiate(varblock, new Vector3(x + Xoff, perlin+0.5f, y + Zoff), Quaternion.identity).transform;
-                blocks[x, y].transform.SetParent(this.transform);
+                var ran = GenerateRandom(x,y, heights);
+                lastHeight = ran;
+                heights[x, y] = ran+1;
+
+                for (int z = 0; z < heights[x, y]; z++)
+                {
+                    GameObject newBlock = Object.Instantiate(varblock, new Vector3(x + Xoff, z + 0.5f, y + Zoff), Quaternion.identity);
+                    newBlock.transform.SetParent(this.transform);
+                }
+
+                //for (int z = 0; z < heights[x, y]; z++)
+                //{
+                //   GameObject newBlock = Object.Instantiate(varblock, new Vector3(x+Xoff, z+0.5f, y+Zoff), Quaternion.identity);
+                //    newBlock.transform.SetParent(this.transform);
+                //}
+
+
                 //blocks[x, y].transform.position = new Vector3(x + Xoff, perlin, y + Zoff);
+
             }
         }
+
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int x = 0; x < dimentions.x; x++)
-        {
-            for (int y = 0; y < dimentions.y; y++)
-            {
-
-                
-
-            }
-        }
+        
 
     }
     private void OnDrawGizmos()
@@ -58,4 +69,35 @@ public class SpawnWalls : MonoBehaviour
         Gizmos.DrawCube(transform.position, Vector3.one);
 
     }
+
+    private int GenerateRandom(int _x, int _y, int[,] _arr)
+    {
+        if (lastHeight == -1) { return Random.Range(0, maxHeight); }
+
+        if (_y == 0)
+        {
+            int ran = Random.Range(lastHeight - 1, lastHeight + 1);
+            Mathf.Clamp(ran, 0, maxHeight);
+            return ran;
+        }
+        else
+        {
+            var adjacent = _arr[_x , _y - 1];
+            int ran = Random.Range(adjacent - 1, adjacent + 1);
+
+            while (ran < adjacent -1 || ran >adjacent +1)
+            {
+                ran = Random.Range(adjacent - 1, adjacent + 1);
+            }
+
+            Mathf.Clamp(ran, 0, maxHeight);
+            return ran;
+
+        }
+
+        //int ran = Random.Range(lastHeight-2, lastHeight+2);
+        //Mathf.Clamp(ran, 0, maxHeight);
+        
+    }
+
 }
